5bf260a6465648a070ef952bca93986a5ee88711
Ryle Fuentes <20197306@s.ubaguio.edu>
2023-03-22 10:58:57 +0800

    fixed the nav

--- resources/views/Master_Layout/welcome_layout.blade.php
+++ resources/views/Master_Layout/welcome_layout.blade.php
@@ -6,26 +6,23 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-
     <link rel="stylesheet" href="@yield('css_design')">
     <title>@yield('title')</title>
 </head>
 <body class="@yield('body-class')">
+
     <!-- Nav tabs -->
-    <ul class="nav nav-tabs" id="navId" role="tablist">
-        <li class="nav-item" role="presentation">
-            <a href="{{url('/')}}" class="nav-link" data-bs-toggle="route" data-bs-target="{{url('/')}}">Welcome</a>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" href="{{url('/')}}">Welcome</a>
         </li>
-       
-        <li class="nav-item" role="presentation">
-            <a href="{{route('login.index')}}" class="nav-link" data-bs-toggle="route" data-bs-target="{{route('login.index')}}">Login</a>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('login.index')}}">Login</a>
         </li>
-       
     </ul>
 
-   
-@yield('body-content')
+    @yield('body-content')
 
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 </body>
 </html>
