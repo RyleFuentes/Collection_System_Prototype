c3eda7bfab6ab77a07cc7af804dbdbd46c7ef641
Ryle Fuentes <20197306@s.ubaguio.edu>
2023-05-03 10:49:57 +0800

    altered the design of the page

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -13,9 +13,9 @@
 @section('body-content')
 
 
-<nav class="navbar bg-dark text-white p-3" data-bs-theme="dark">
+<nav class="navbar text-white p-3">
     <a href=""   class="nav-link"><h3>Editor</h3></a>
-    <div class="nav-content d-flex" style="gap: 1.5em">
+    <div class="nav-content nav-div d-flex" style="gap: 1.5em">
         <a href="" class="nav-link">History</a>
         <a href="{{route('logout')}}" class="nav-link">Logout</a>
     </div>
@@ -28,20 +28,31 @@
 @endif
 
 <div class="container-fluid table_container mt-5">
+    <div class="container search ">
+        <input type="text" class="form-control search-bar">
+        <button class="btn search-btn btn-dark" type="submit">Search</button>
+    </div>
     <table class="table table-success table_test">
         <thead>
             <th scope="col">Name</th>
             <th scope="col">Running balance</th>
             <th scope="col">Extra balance</th>
+            <th scope="col">Action</th>
         </thead>
 
         <tbody class="text-black">
             @foreach ($members as $item)
                 <tr>
+                    
                     <td>{{$item->FirstName}}</td>
                     <td>test</td>
                     <td>Hello</td>
-                </tr>
+                    
+                    <td>
+                        <button type="submit" class="btn updateBtn btn-outline-dark">Update</button>
+                        <button type="submit" class="btn deleteBtn btn-outline-dark">Delete</button> 
+                    </td>
+                </tr> 
                 
             @endforeach
                 
